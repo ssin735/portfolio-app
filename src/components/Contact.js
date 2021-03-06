b8d188fe9cf3c41b2117a020702bac5f61af5feb
ssin735 <ssin735@aucklanduni.ac.nz>
2021-03-07 11:45:04 +1300

    Contact Page functionality added

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,19 +1,53 @@
-import React from "react";
+import React,{useState} from "react";
+import emailjs from 'emailjs-com';
+import {useForm} from "react-hook-form";
 
 const Contacts = () => {
-    return (
 
+    const [successMessage, setSuccessMessage] = useState("");
+    const{register,handleSubmit,errors} = useForm();
+
+    const serviceID = "service_ifarlgd";
+    const templateID = "template_tempID";
+    const userID = "user_FYBLGJgOnABW3LSl9izbs";
+
+    const onSubmit = (data, r) => {
+        sendEmail(serviceID,
+            templateID,
+            {
+                name:data.name,
+                phone:data.phone,
+                email:data.email,
+                subject:data.subject,
+                description:data.description
+            },
+            userID )
+
+            r.target.reset();
+    }
+
+    const sendEmail = (serviceID, templateID, variables, userID) => {
+        emailjs.send(serviceID, templateID, variables, userID)
+          .then(() => {
+              setSuccessMessage("Thank You, Your message was sent successfully! I'll contact you as soon as possible.");
+          }).catch(err => console.error('Something went wrong ${err}'));
+      }
+
+
+
+    return (
         <div className="contacts">
             <div className="text-center">
             <h1> Contact Me </h1>
             <p>Please fill out the form to contact me.</p>
+
+            <span className="success-message">{successMessage}</span>
             </div>
 
 
         <div className="container">
-
+        <form onSubmit={handleSubmit(onSubmit)}>
             <div className="row">
-
                 <div className = "col-md-6 col-xs-12">
                     {/* Name input */}
 
@@ -23,27 +57,65 @@ const Contacts = () => {
                     className="form-control"
                     placeholder = "Your Name"
                     name="name"
+                    ref={
+                        register({
+                            required:"Please enter your name.",
+                            maxLength:{
+                                value:20,
+                                message:"Please enter a name with less than 20 characters."
+                            }
+                            
+                        })
+                    }
                     />
                     <div className="line"></div>
                     </div>
+                    <span className="error-message">
+                        {errors.name && errors.name.message}
+                    </span>
 
                     {/* Phone number  */}
                     <div className="text-center">
-                    <input type="text" className="form-control" placeholder = "Phone Number" phone="phone" />
+                    <input type="text" className="form-control" placeholder = "Phone Number" 
+                    name="phone" 
+                    ref={
+                        register({
+                            required:"Please provide your phone number."
+                        })
+                    }
+                    />
                     <div className="line"></div>
                     </div>
+                    <span className="error-message">
+                        {errors.phone && errors.phone.message}
+                    </span>
 
                     {/* Email  */}
                     <div className="text-center">
                     <input type="email" className="form-control" placeholder = "Email"
-                    email="email"/>
+                    name="email"
+                    ref={
+                        register({
+                            required:"Please enter your email address.",
+                            pattern:{
+                                value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                                message:"email is either incomplete or invalid."
+                            }
+                            
+                        })
+                    }
+                    />
                     <div className="line"></div>
                     </div>
+                    <span className="error-message">
+                        {errors.email && errors.email.message}
+                    </span>
 
                     {/* Subject  */}
                     <div className="text-center">
                     <input
-                    type="text" className="form-control" placeholder = "Subject" subject="subject"/>
+                    type="text" className="form-control" placeholder = "Subject"
+                    name="subject"/>
                      <div className="line"></div>
                      </div>
 
@@ -57,7 +129,7 @@ const Contacts = () => {
                     type = "text"
                     className="form-control"
                     placeholder = "Please describe your project requirements."
-                    desciption="description"
+                    name="description"
                     /> 
                     <div className="line"></div>
                     </div>
@@ -65,6 +137,7 @@ const Contacts = () => {
                     <button className = "btn-main-offer contacts-btn" type="submit"> Submit </button>
                 </div>
             </div>
+        </form>
         </div>
     </div>
     )
